dc0ee92a46dd84ac7da992129471f08d0b57825e
DineshPrakasam <dinesh.prakasam@aviartechservices.com>
2022-10-11 11:03:56 +0530

    [UB-F]added heroku link

--- src/Api.js
+++ src/Api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_URL = 'http://localhost:3005/';
+// const API_URL = 'http://localhost:3005/';
+ const API_URL = 'https://user-blog-backend.herokuapp.com/'
 
 export default axios.create({
   baseURL: API_URL,
